81227442a5eaa36609dd1bcfae70af74eb1cb5b8
Paulo Rodrigues <pauloadesr@gmail.com>
2020-05-23 21:56:37 -0300

    base project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import './config/ReactotronConfig';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Olá</Text>
+    </View>
+  );
+}
